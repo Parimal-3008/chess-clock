311316fe9afdbf825163aa5c371ad61a040cf23e
Parimal-3008 <parimalyeole30@gmail.com>
2021-10-12 22:21:21 +0530

    Time control feature is added with time inc. Frontend remaining

--- time123.js
+++ time123.js
@@ -1,6 +1,11 @@
 let p2 = document.getElementById("timer2");
 
-p2.innerText = localStorage.getItem("Format");
+
+if (localStorage.getItem("Format") == "Choose the game format")
+    p2.innerText = "BLITZ / RAPID";
+else
+    p2.innerText = localStorage.getItem("Format");
+
 let wi = (localStorage.getItem("whiteInitial"));
 let bi = (localStorage.getItem("blackInitial"));
 let binc = (localStorage.getItem("blackInc1"));
@@ -33,19 +38,29 @@ function cj() {
 
         };
         console.log(mo);
+        if (mo == moves1 * 2) {
+            b.time += (parseInt(b.at2)*60);
+            a.time += (parseInt(a.at2)*60);
+            f1();
+            f2();
+            a.inc1 = a.inc2;
+            b.inc1 = b.inc2;
+
+        }
+        if (mo == moves2 * 2) {
+            b.time += (parseInt(b.at3)*60);
+            a.time += (parseInt(a.at3)*60);
+            f1();
+            f2();
+            a.inc1 = a.inc3;
+            b.inc1 = b.inc3;
+
+        }
 
         if (mo % 2 == 1) {
             if (mo != 1) {
-                b.time += parseInt(b.inc1);
-                min1 = Math.floor(b.time / 60);
-                sec1 = b.time % 60;
-                if (min1 < 10)
-                    min1 = "00" + min1;
-                else if (min1 < 100)
-                    min1 = "0" + min1;
-                if (sec1 < 10)
-                    sec1 = "0" + sec1;
-                document.getElementById("BLACK").innerText = min1 + " " + sec1;
+                b.time += (parseInt(b.inc1) + 1);
+                f1();
             }
             yo = setInterval(whitecountdown, 1000);
             clearInterval(yu);
@@ -53,17 +68,8 @@ function cj() {
         }
         else {
             if (mo != 0) {
-                a.time += parseInt(a.inc1);
-                min = Math.floor(a.time / 60);
-                sec = a.time % 60;
-                if (min < 10)
-                    min = "00" + min;
-                else if (min < 100)
-                    min = "0" + min;
-                if (sec < 10)
-                    sec = "0" + sec;
-                document.getElementById("WHITE").innerText = min + " " + sec;
-                document.getElementById("WHITE").innerText = min + " " + sec;
+                a.time += (parseInt(a.inc1) + 1);
+                f2();
             }
             yu = setInterval(blackcountdown, 1000);
             clearInterval(yo);
@@ -71,18 +77,15 @@ function cj() {
     };
 }
 function whitecountdown() {
-    min = Math.floor(a.time / 60);
-    sec = a.time % 60;
+    f2();
     a.time--;
-    if (min < 10)
-        min = "00" + min;
-    else if (min < 100)
-        min = "0" + min;
-    if (sec < 10)
-        sec = "0" + sec;
-    document.getElementById("WHITE").innerText = min + " " + sec;
 }
 function blackcountdown() {
+    f1();
+    b.time--;
+
+}
+function f1() {
     min1 = Math.floor(b.time / 60);
     sec1 = b.time % 60;
     if (min1 < 10)
@@ -91,6 +94,17 @@ function blackcountdown() {
         min1 = "0" + min1;
     if (sec1 < 10)
         sec1 = "0" + sec1;
-    b.time--;
     document.getElementById("BLACK").innerText = min1 + " " + sec1;
+}
+function f2() {
+    min = Math.floor(a.time / 60);
+    sec = a.time % 60;
+    if (min < 10)
+        min = "00" + min;
+    else if (min < 100)
+        min = "0" + min;
+    if (sec < 10)
+        sec = "0" + sec;
+    document.getElementById("WHITE").innerText = min + " " + sec;
+
 }
