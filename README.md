248c9a45fe26fe01fdeea0dc29c9e336bf8e034d
Parimal-3008 <parimalyeole30@gmail.com>
2021-10-13 23:32:27 +0530

    1st page frontend done and working on edge cases

---
+++ README.md
@@ -0,0 +1,10 @@
+# chess-clock
+Not every chess player can have a chess clock for over the board games
+So in this project I have made a chess clock.
+Features:
+-> Players can choose game format they want to play
+-> They can set their initial time as well as increment time 
+-> There is also the feature of time control if you wants to play a classical game 
+-> If a player thinks he is playing against a relatively weak opponent then he/she can choose whether he wants an increment in time after playing a certain move or not or even can set less time depending what he/she wants.
+-> After making a move player has to just press SPACE BAR key just like they press the keys of chess clocks.
+-> The liberty of time setting is pretty useful in Armageddon format.
--- home2.js
+++ home2.js
@@ -1,35 +1,29 @@
 
-function optionchoosed( gameformat) {
+function optionchoosed(gameformat) {
     let p1 = document.getElementById("time");
-    
+
     // console.log(p1.innerHTML);
     p1.innerText = gameformat;
     toggledisplay();
-   
-    if(gameformat == "Classical")
-    {
-        for( let i=0;i<  document.getElementsByClassName("TimeControl1").length;i++)
-        {
-            document.getElementsByClassName("TimeControl1")[i].style.visibility="visible";
+
+    if (gameformat == "Classical") {
+        for (let i = 0; i < document.getElementsByClassName("TimeControl1").length; i++) {
+            document.getElementsByClassName("TimeControl1")[i].style.visibility = "visible";
         }
-        for( let i=0;i<  document.getElementsByClassName("TimeControl2").length;i++)
-        {
-            document.getElementsByClassName("TimeControl2")[i].style.visibility="visible";
+        for (let i = 0; i < document.getElementsByClassName("TimeControl2").length; i++) {
+            document.getElementsByClassName("TimeControl2")[i].style.visibility = "visible";
         }
     }
-    else
-    {
-        for( let i=0;i<  document.getElementsByClassName("TimeControl1").length;i++)
-        {
-            document.getElementsByClassName("TimeControl1")[i].style.visibility="hidden";
+    else {
+        for (let i = 0; i < document.getElementsByClassName("TimeControl1").length; i++) {
+            document.getElementsByClassName("TimeControl1")[i].style.visibility = "hidden";
         }
-        for( let i=0;i<  document.getElementsByClassName("TimeControl2").length;i++)
-        {
-            document.getElementsByClassName("TimeControl2")[i].style.visibility="hidden";
+        for (let i = 0; i < document.getElementsByClassName("TimeControl2").length; i++) {
+            document.getElementsByClassName("TimeControl2")[i].style.visibility = "hidden";
         }
 
     }
-    
+
 }
 let rotation = 0;
 function toggledisplay() {
@@ -44,22 +38,16 @@ function toggledisplay() {
     r3();
     MOVE();
 }
-function r3()
-{
-    img=document.getElementById("arrow");
-   if(img.style.transform == "rotate(180deg)")
-   {
-      img.style.transform = "rotate(0deg)";
-   }    
-    else
-    {
+function r3() {
+    img = document.getElementById("arrow");
+    if (img.style.transform == "rotate(180deg)") {
+        img.style.transform = "rotate(0deg)";
+    }
+    else {
         img.style.transform = "rotate(180deg)";
         img.style.padding = "12px";
-
     }
-    img.style.transition= "transform 0.1s ease ";
-    
-    
+    img.style.transition = "transform 0.1s ease ";
 }
 function handleChange(input) {
     if (input.value < 0) input.value = 0;
@@ -82,43 +70,49 @@ function handleChange5(input) {
     if (input.value > 80) input.value = 80;
 }
 
-function MOVE()
-{
+function MOVE() {
      let p1 = document.getElementById("time");
-     localStorage.setItem("Format", p1.innerText);
-     console.log(p1.innerText);
+     if (document.getElementById("WIT").value.trim().length == 0 || document.getElementById("WIncT").value .trim().length == 0  || document.getElementById("BIT").value .trim().length == 0 || document.getElementById("BIncT").value.trim().length == 0 ) {
+        alert("Please fill all the required values");
+        return false;  
+    }
+    else
+    {
+        window.location.href="timer.html";
+    }
+    localStorage.setItem("Format", p1.innerText);
+    console.log(p1.innerText);
+   localStorage.setItem("whiteInitial", document.getElementById("WIT").value);
+    localStorage.setItem("whiteInc1", document.getElementById("WIncT").value);
+    localStorage.setItem("blackInitial", document.getElementById("BIT").value);
+    localStorage.setItem("blackInc1", document.getElementById("BIncT").value);
+    console.log(document.getElementById("WIT").value);
+    if (p1.innerText == "Classical") {
+        localStorage.setItem("NoOfMoves1", document.getElementById("tc1").value);
+        localStorage.setItem("WAT2", document.getElementById("WAT2").value);
+        localStorage.setItem("BAT2", document.getElementById("BAT2").value);
+        localStorage.setItem("WIncT2", document.getElementById("WIncT2").value);
+        localStorage.setItem("BIncT2", document.getElementById("BIncT2").value);
+        localStorage.setItem("NoOfMoves2", document.getElementById("tc2").value);
+        localStorage.setItem("WAT3", document.getElementById("WAT3").value);
+        localStorage.setItem("BAT3", document.getElementById("BAT3").value);
+        localStorage.setItem("WIncT3", document.getElementById("WIncT3").value);
+        localStorage.setItem("BIncT3", document.getElementById("BIncT3").value);
 
-     localStorage.setItem("whiteInitial", document.getElementById("WIT").value );
-     localStorage.setItem("whiteInc1", document.getElementById("WIncT").value );
-     localStorage.setItem("blackInitial", document.getElementById("BIT").value );
-     localStorage.setItem("blackInc1", document.getElementById("BIncT").value );
-     console.log(document.getElementById("WIT").value);
-     if(p1.innerText == "Classical")
-     {
-        localStorage.setItem("NoOfMoves1",document.getElementById("tc1").value);
-        localStorage.setItem("WAT2",document.getElementById("WAT2").value);
-        localStorage.setItem("BAT2",document.getElementById("BAT2").value);
-        localStorage.setItem("WIncT2",document.getElementById("WIncT2").value);
-        localStorage.setItem("BIncT2",document.getElementById("BIncT2").value);
-        localStorage.setItem("NoOfMoves2",document.getElementById("tc2").value);
-        localStorage.setItem("WAT3",document.getElementById("WAT3").value);
-        localStorage.setItem("BAT3",document.getElementById("BAT3").value);
-        localStorage.setItem("WIncT3",document.getElementById("WIncT3").value);
-        localStorage.setItem("BIncT3",document.getElementById("BIncT3").value);
-
-     }
-     else
-     {
-        localStorage.setItem("NoOfMoves1",-1);
-        localStorage.setItem("WAT2",-1);
-        localStorage.setItem("BAT2",-1);
-        localStorage.setItem("WIncT2",-1);
-        localStorage.setItem("BIncT2",-1);
-        localStorage.setItem("NoOfMoves2",-1);
-        localStorage.setItem("WAT3",-1);
-        localStorage.setItem("BAT3",-1);
-        localStorage.setItem("WIncT3",-1);
-        localStorage.setItem("BIncT3",-1);
-     }
+    }
+    else {
+        localStorage.setItem("NoOfMoves1", -1);
+        localStorage.setItem("WAT2", -1);
+        localStorage.setItem("BAT2", -1);
+        localStorage.setItem("WIncT2", -1);
+        localStorage.setItem("BIncT2", -1);
+        localStorage.setItem("NoOfMoves2", -1);
+        localStorage.setItem("WAT3", -1);
+        localStorage.setItem("BAT3", -1);
+        localStorage.setItem("WIncT3", -1);
+        localStorage.setItem("BIncT3", -1);
+    }
     
+    
+
 }
